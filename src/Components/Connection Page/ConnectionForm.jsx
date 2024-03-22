4b4e6ae56ac4f46332df47b99077cda0a0a4e128
DIVUM <ravichandran.shankar@divum.in>
2024-03-22 14:22:01 +0530

    Connection page completed

--- src/Components/Connection Page/ConnectionForm.jsx
+++ src/Components/Connection Page/ConnectionForm.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 
-const ConnectionForm = () => {
+const ConnectionForm = ({ connection, setConnection }) => {
   return (
-    <div>
-        
+    <div className='connectionFormTemplate'>
+      <p className='heading'>Add Connection</p>
+
+      <div className='connectionFormContainer'>
+
+        <div className='inputTab'>
+          <p className='label'>Username</p>
+          <input type="text" className='nameInput' value={connection.username} onChange={(e)=>setConnection(prev=> ({...prev, username: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Password</p>
+          <input type="text" className='nameInput' value={connection.password} onChange={(e)=>setConnection(prev=> ({...prev, password: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Source DB Name</p>
+          <input type="text" className='nameInput' value={connection.sourceDbName} onChange={(e)=>setConnection(prev=> ({...prev, sourceDbName: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Source DB Table Name</p>
+          <input type="text" className='nameInput' value={connection.sourceDbTableName} onChange={(e)=>setConnection(prev=> ({...prev, sourceDbTableName: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Destination DB Name</p>
+          <input type="text" className='nameInput' value={connection.destinationDbName} onChange={(e)=>setConnection(prev=> ({...prev, destinationDbName: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Destination DB Table Name</p>
+          <input type="text" className='nameInput' value={connection.destinationDbTableName} onChange={(e)=>setConnection(prev=> ({...prev, destinationDbTableName: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Host</p>
+          <input type="text" className='nameInput' value={connection.host} onChange={(e)=>setConnection(prev=> ({...prev, host: e.target.value}))}/>
+        </div>
+
+        <div className='inputTab'>
+          <p className='label'>Port</p>
+          <input type="text" className='nameInput' value={connection.port} onChange={(e)=>setConnection(prev=> ({...prev, port: e.target.value}))}/>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/Components/Connection Page/ConnectionPage.jsx
+++ src/Components/Connection Page/ConnectionPage.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react'
 import ProgressionBar from '../Progression Bar/ProgressionBar'
 import Button from '../Button/Button'
+import ConnectionForm from './ConnectionForm'
+import './connectionPage.scss'
 
 const ConnectionPage = ({ setPage }) => {
 
     const [connection, setConnection] = useState({
-        hello: '',
-        world: ''
+        username: '',
+        password: '',
+        sourceDbName: '',
+        sourceDbTableName: '',
+        destinationDbName: '',
+        destinationDbTableName: '',
+        host: '',
+        port: ''
     })
 
     return (
         <div className='actionArea'>
             <ProgressionBar level={2} />
+            <ConnectionForm connection={connection} setConnection={setConnection}/>
 
             <div className='actionButtons' style={{ justifyContent: "right" }}>
                 <div onClick={() => setPage('toolDetails')}><Button content="Previous Step" type="outline" /></div>
-                {(connection.hello && connection.world) ? <div><Button content="Add Connection" type="fill" /></div> :
+                {(connection.username && connection.password && connection.sourceDbName && connection.sourceDbTableName && connection.destinationDbName && connection.destinationDbTableName && connection.host && connection.port) ? <div><Button content="Add Connection" type="fill" /></div> :
 
                     <div className='disabledBtn'>
                         <p>Add Connection</p>
