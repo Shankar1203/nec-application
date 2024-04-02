bee9452a6315ee292b60cece74d086aa7a6b46dc
Shankar1203 <ravichandran.shankar@divum.in>
2024-04-02 11:09:28 +0530

    ip updated

--- src/Api/ip.js
+++ src/Api/ip.js
@@ -1,2 +1,2 @@
 // export const ip = 'https://7341-106-51-80-105.ngrok-free.app/';
-export const ip = 'http://192.168.0.128:5000';
+export const ip = 'https://8c95163788d7d6ffabd40a2230e7af9e.loophole.site';
